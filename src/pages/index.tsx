0e3c6974f3476e4dabda3ff2d8faa64459a56248
jennifertakagi <jennitakagi@gmail.com>
2021-05-20 18:58:11 -0300

    feat: add global styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,5 @@
 import Head from 'next/head';
 
-import styles from '../styles/home.module.scss';
-
 export default function Home() {
   return (
     <>
@@ -9,7 +7,7 @@ export default function Home() {
         <title>Home | IgNews</title>
       </Head>
       
-      <h1 className={styles.title}>IgNews</h1>
+      <h1>IgNews</h1>
     </>
   )
 }
