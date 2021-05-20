0f25b094fbcef45f126134a1672909ff03f5081f
jennifertakagi <jennitakagi@gmail.com>
2021-05-20 18:46:43 -0300

    feat: add dynamic title to each page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,15 @@
+import Head from 'next/head';
+
 import styles from '../styles/home.module.scss';
 
 export default function Home() {
   return (
-    <h1 className={styles.title}>IgNews</h1>
+    <>
+      <Head>
+        <title>Home | IgNews</title>
+      </Head>
+      
+      <h1 className={styles.title}>IgNews</h1>
+    </>
   )
 }
