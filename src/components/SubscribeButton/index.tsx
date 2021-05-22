7eed45c739a8baf33b853e3660cd2764145f0ee6
jennifertakagi <jennitakagi@gmail.com>
2021-05-22 17:32:21 -0300

    feat: get priceId on SubscribeButton as prop

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,6 +1,12 @@
 import styles from './styles.module.scss';
 
-export function SubscribeButton(): JSX.Element {
+interface SubscribeButtonProps {
+  priceId: string;
+}
+
+export function SubscribeButton({
+  priceId,
+}: SubscribeButtonProps): JSX.Element {
   return (
     <button type="button" className={styles.subscribeButton}>
       Subscribe now
