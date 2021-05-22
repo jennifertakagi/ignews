b48bd5b48a9645e8dcdb12828a9780a4cde45ff2
jennifertakagi <jennitakagi@gmail.com>
2021-05-22 16:53:07 -0300

    feat: use SignInButton on Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,5 @@
+import { SignInButton } from '../SignInButton';
+
 import styles from './styles.module.scss';
 
 export function Header(): JSX.Element {
@@ -11,6 +13,7 @@ export function Header(): JSX.Element {
           </a>
           <a href="#">Posts</a>
         </nav>
+        <SignInButton />
       </div>
     </header>
   );
