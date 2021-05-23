a19729c8ef6b9f3e7e93d35ce87c1f0cfd0a45ae
jennifertakagi <jennitakagi@gmail.com>
2021-05-23 18:46:58 -0300

    feat: add configuration to next-auth to github

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import { AppProps } from 'next/app';
+import { Provider as NextAuthProvider } from 'next-auth/client';
 
 import '../styles/global.scss';
 
@@ -6,10 +7,10 @@ import { Header } from '../components/Header';
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
   return (
-    <>
+    <NextAuthProvider session={pageProps.session}>
       <Header />
       <Component {...pageProps} />
-    </>
+    </NextAuthProvider>
   );
 }
 
