e5faab6b48e6f63e80a8a81af60f6c6f80806f47
jennifertakagi <jennitakagi@gmail.com>
2021-05-24 08:22:07 -0300

    feat: add user email into Fauna DB

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,8 @@
+import { query } from 'faunadb';
 import NextAuth from 'next-auth';
 import Providers from 'next-auth/providers';
+
+import { fauna } from '../../../services/fauna';
 
 export default NextAuth({
   providers: [
@@ -9,4 +12,32 @@ export default NextAuth({
       scope: 'read:user',
     }),
   ],
+  jwt: {
+    signingKey: process.env.SIGNIN_KEY,
+  },
+  callbacks: {
+    async signIn(user) {
+      const { email } = user;
+
+      try {
+        await fauna.query(
+          query.If(
+            query.Not(
+              query.Exists(
+                query.Match(
+                  query.Index('user_by_email', query.Casefold(user.email)),
+                ),
+              ),
+            ),
+            query.Create(query.Collection('users'), { data: { email } }),
+            query.Get(query.Index('user_by_email', query.Casefold(user.email))),
+          ),
+        );
+
+        return true;
+      } catch {
+        return false;
+      }
+    },
+  },
 });
