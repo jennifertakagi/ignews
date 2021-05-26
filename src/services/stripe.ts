93805517955669985c64fcb351508b6105fd6b87
jennifertakagi <jennitakagi@gmail.com>
2021-05-26 08:17:32 -0300

    feat: add configuration to axios and stripe.js

--- src/services/stripe.ts
+++ src/services/stripe.ts
@@ -1,7 +1,7 @@
 import Stripe from 'stripe';
 import { version } from '../../package.json';
 
-export const stripe = new Stripe(process.env.STRIPE_API_KEY, {
+export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
   apiVersion: '2020-08-27',
   appInfo: {
     name: 'IgNews',
