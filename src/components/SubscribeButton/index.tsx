20fb3ad39c1298c97eca973bad2d97a3bad77e2d
jennifertakagi <jennitakagi@gmail.com>
2021-05-26 08:17:56 -0300

    feat: add subscribe behavior

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,3 +1,8 @@
+import { signIn, useSession } from 'next-auth/client';
+import Stripe from 'stripe';
+import { api } from '../../services/api';
+import { getStripeJs } from '../../services/stripe.js';
+
 import styles from './styles.module.scss';
 
 interface SubscribeButtonProps {
@@ -7,8 +12,31 @@ interface SubscribeButtonProps {
 export function SubscribeButton({
   priceId,
 }: SubscribeButtonProps): JSX.Element {
+  const [session] = useSession();
+
+  async function handleSubscribe(): Promise<Stripe> {
+    if (!session) {
+      signIn('github');
+      return;
+    }
+
+    try {
+      const response = await api.post('/subscribe');
+      const { sessionId } = response.data;
+      const stripe = await getStripeJs();
+
+      await stripe.redirectToCheckout({ sessionId });
+    } catch (error) {
+      alert(error.message);
+    }
+  }
+
   return (
-    <button type="button" className={styles.subscribeButton}>
+    <button
+      type="button"
+      className={styles.subscribeButton}
+      onClick={handleSubscribe}
+    >
       Subscribe now
     </button>
   );
