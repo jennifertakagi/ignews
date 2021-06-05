af095d618f564d86d0b3a7ef1e53dceb831e2bb0
jennifertakagi <jennitakagi@gmail.com>
2021-06-05 19:28:49 -0300

    feat: get user activate and logged on FaunaDB

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -16,6 +16,33 @@ export default NextAuth({
     signingKey: process.env.SIGNIN_KEY,
   },
   callbacks: {
+    async session(session) {
+      try {
+        const userActiveSubscription = await fauna.query(
+          query.Get(
+            query.Intersection([
+              query.Match(
+                query.Index('subscription_by_user_ref'),
+                query.Select(
+                  'ref',
+                  query.Get(
+                    query.Match(
+                      query.Index('user_by_email'),
+                      query.Casefold(session.user.email),
+                    ),
+                  ),
+                ),
+              ),
+              query.Match(query.Index('subscription_by_status'), 'active'),
+            ]),
+          ),
+        );
+
+        return { ...session, activeSubscription: userActiveSubscription };
+      } catch {
+        return { ...session, activeSubscription: null };
+      }
+    },
     async signIn(user) {
       const { email } = user;
 
