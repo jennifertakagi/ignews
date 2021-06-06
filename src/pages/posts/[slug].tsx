a97e483b453a9a8d1f42fe186c7558b2d0a82306
jennifertakagi <jennitakagi@gmail.com>
2021-06-06 20:11:34 -0300

    feat: add PostPreview component and style it

--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -42,7 +42,7 @@ export const getServerSideProps: GetServerSideProps = async ({
 }) => {
   const session = await getSession({ req });
 
-  if (!session.activeSubscription) {
+  if (!session?.activeSubscription) {
     return {
       redirect: {
         destination: '/',
